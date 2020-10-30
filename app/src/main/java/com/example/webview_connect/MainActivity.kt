0af53e9a407ba17a9a2b43381296af950541cf42
FloreErin <floreerin@gmail.com>
2020-10-30 10:00:20 +0900

    Add Rxjava for Async

--- app/src/main/java/com/example/webview_connect/MainActivity.kt
+++ app/src/main/java/com/example/webview_connect/MainActivity.kt
@@ -3,8 +3,14 @@ package com.example.webview_connect
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.webkit.JavascriptInterface
 import android.widget.Toast
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,23 +20,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        wv_welcome.run{
+        wv_welcome.run {
             settings.javaScriptEnabled = true
             loadUrl("file:///android_asset/welcome.html")
         }
 
-        wv_welcome.addJavascriptInterface(object : JsCallBack{
+        // js -> native (웹 코드에서 안드로이드 동작을 유도)
+        wv_welcome.addJavascriptInterface(object : JsCallBack {
+            @SuppressLint("CheckResult")
             @JavascriptInterface
             override fun webViewToApp(value: String?) {
-                // js -> native 로 온 결과값 세팅
-                tv_return.text = value
+                Single.just(value)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(
+                        {
+                            tv_return.text = it
+                        },
+                        {
+                            Log.d("TAG", "error code : $it")
+                        }
+                    )
             }
         }, "WebViewCallbackInterface")
 
+        // native -> js (안드로이드 코드에서 웹 동작을 유도)
         btn_nativeCallBack.setOnClickListener {
-            val textMessage : String = et_text.text.toString()
+            val textMessage: String = et_text.text.toString()
             wv_welcome.evaluateJavascript("javascript:appToWebView('$textMessage')") {
-                value -> Toast.makeText(this, "js 전송 성공 : $value", Toast.LENGTH_LONG).show()
+                // 콜백
+                    value ->
+                Toast.makeText(this, "js 전송 성공 : $value", Toast.LENGTH_LONG).show()
             }
         }
     }
